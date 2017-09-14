89a3330880a87fc3617d2df39b2dffcaf09a835a
NgocTam <ngoctam3096@gmail.com>
2017-09-14 09:13:01 +0700

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    render html: "xin chào cả thế giới, world! "
   end
 end
